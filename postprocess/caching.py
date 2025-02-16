ef97cc6de373daeff549f0ca994176388f0644f8
Mike <yeyuting0307@gmail.com>
2025-02-17 00:19:08 +0800

    abstract rerank and cache process

---
+++ postprocess/caching.py
@@ -0,0 +1,12 @@
+#%%
+import requests
+import aiohttp
+import asyncio
+
+def cache_to_memory_store(*args, **kwargs):
+    ''' 將推薦結果部分暫存以供快取 '''
+    ...
+
+async def cache_to_memory_store_async(*args, **kwargs):
+    ''' (非同步)將推薦結果部分暫存以供快取 '''
+    ...
